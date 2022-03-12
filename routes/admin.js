f07cd0d82ae06108b6ee29996ea7b5f3b66ae757
ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-12 20:58:15 +0100

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const routerAdmin=express.Router();
+const pathDir=require("../util/path");
+const path=require("path")
+
+// products
+const products=[]
+
+routerAdmin.get("/add-product",(req, res, next) => {
+    res.render("add-product", {title:"add-product"})
+})
+routerAdmin.post("/add-product",(req,res,next)=>{
+    products.push({title:req.body.title,price:req.body.price, path:"admin/add-product"})
+    res.redirect("/")
+})
+
+module.exports={routerAdmin,products}
